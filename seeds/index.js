20dded66b97cac456a8727926503625e9b221ced
Eric Choo <ericchoo_0520@yahoo.com>
2021-11-10 01:31:12 +0800

    add authorization

--- seeds/index.js
+++ seeds/index.js
@@ -22,6 +22,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author:'618a3fbcb287b81942868fad',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
@@ -35,3 +36,4 @@ const seedDB = async () => {
 seedDB().then(() => {
     mongoose.connection.close();
 })
+
